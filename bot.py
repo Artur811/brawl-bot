5b58f25d7d5959b8909b2c4bac917294368363bc
Artur811 <mikayelyanartur661@gmail.com>
2026-05-08 03:47:51 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def start(message: types.Message):
             [
                 InlineKeyboardButton(
                     text="🛒 Купить",
-                    url="https://funpay.com/users/19690950/"
+                    url="http://t.me/bsfunpaysw1zy_bot/myapp"
                 )
             ]
         ]
